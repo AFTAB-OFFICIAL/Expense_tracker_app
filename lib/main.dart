6d959426b80853c4fbe4e735fcfa95786b658479
Md Aftab <mdaftab@Mds-MacBook-Air.local>
2024-01-27 23:57:07 +0530

    added theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(
+    255,
+    96,
+    59,
+    181,
+  ),
+);
+
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Expenses(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.all(
+            10,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: TextTheme().copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.secondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  );
 }
